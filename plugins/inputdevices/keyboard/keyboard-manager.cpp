126ce9f8cedacf24074ea8cdb77d89070a41b39e
liuxinhao <liuxinhao@kylinsec.com.cn>
2025-09-02 16:25:20 +0800

    fix(keyboard): Fix issue caused by delayed synchronization when disabling repeat keys
    - 修复关闭重复键未及时同步导致问题
    
    Fixed #90671

--- plugins/inputdevices/keyboard/keyboard-manager.cpp
+++ plugins/inputdevices/keyboard/keyboard-manager.cpp
@@ -453,6 +453,8 @@ void KeyboardManager::set_auto_repeat()
     {
         XAutoRepeatOff(GDK_DISPLAY_XDISPLAY(display));
     }
+
+    XFlush(display);
 }
 
 bool KeyboardManager::set_layouts(const std::vector<Glib::ustring> &layouts)
